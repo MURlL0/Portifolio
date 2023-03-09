6b458e3c8403c77e366e220377f8a09200174ac9
MURlL0 <muriilorodrigues@outlook.com>
2023-03-09 01:34:27 -0300

    registro/falta o popup para o login amanha faço

---
+++ Sites de logins/Resgistro/js/script.js
@@ -0,0 +1,31 @@
+function validar() {
+    if (document.login.user.value == "") {
+        document.getElementById("result").innerHTML = "Enter Username";
+        return false;
+    }
+    if (document.login.user.value.length <6) {
+        document.getElementById("result").innerHTML = "enter 6 digits minimum";
+        return false;
+    }
+    if (document.login.email.value == "") {
+        document.getElementById("result").innerHTML = "Enter Email";
+        return false;
+    }
+    if (document.login.password.value == "") {
+        document.getElementById("result").innerHTML = "Enter Password";
+        return false;
+    }
+    if (document.login.password.value.length < 6) {
+        document.getElementById("result").innerHTML = "Enter 6 digits minimum";
+        return false;
+    }
+    if (document.login.cpassword.value == "") {
+        document.getElementById("result").innerHTML =
+            "Enter password confirmation";
+        return false;
+    }
+    if (document.login.password.value !== document.login.cpassword.value) {
+        document.getElementById("result").innerHTML = "Different password";
+        return false;
+    }
+}
