292c23a4dcea421d35a074ee12139c9440df7507
MURlL0 <muriilorodrigues@outlook.com>
2023-04-19 12:20:07 -0300

    apagar o menu mobile e fazer denovo

---
+++ Sites de logins/Login-menu/js/script.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('#menu');
+
+function toggleMenu() {
+    const dropdown = document.querySelector('#dropdown');
+    dropdown.classList.toggle('active')
+}
+
+menu.addEventListener('click', toggleMenu);
