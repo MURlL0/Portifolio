ee9195acc977a227e0253e21853122bb93ce226d
MURlL0 <muriilorodrigues@outlook.com>
2023-04-19 16:52:34 -0300

    pronto login-menu/register ajustar-sass

--- Sites de logins/Login-menu/js/script.js
+++ Sites de logins/Login-menu/js/script.js
@@ -2,7 +2,7 @@ const menu = document.querySelector('#menu');
 
 function toggleMenu() {
     const dropdown = document.querySelector('#dropdown');
-    dropdown.classList.toggle('active')
+    dropdown.classList.toggle('active');
 }
 
 menu.addEventListener('click', toggleMenu);
