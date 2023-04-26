93512672bd7a9a6a1d52cd2697b9b53c07afb902
MURlL0 <muriilorodrigues@outlook.com>
2023-04-25 21:23:19 -0300

    site pronto/responsivo

--- Sites de logins/login-future/js/script.js
+++ Sites de logins/login-future/js/script.js
@@ -1,40 +1,40 @@
 function validar() {
-    if (document.login.user.value == "") {
-        document.getElementById("result").innerHTML = "Enter Username";
+    if (document.login.user.value == '') {
+        document.getElementById('result').innerHTML = 'Enter Username';
         return false;
     }
     if (document.login.user.value.length < 6) {
-        document.getElementById("result").innerHTML = "enter 6 digits minimum";
+        document.getElementById('result').innerHTML = 'enter 6 digits minimum';
         return false;
     }
-    if (document.login.email.value == "") {
-        document.getElementById("result").innerHTML = "Enter Email";
+    if (document.login.email.value == '') {
+        document.getElementById('result').innerHTML = 'Enter Email';
         return false;
     }
-    if (document.login.password.value == "") {
-        document.getElementById("result").innerHTML = "Enter Password";
+    if (document.login.password.value == '') {
+        document.getElementById('result').innerHTML = 'Enter Password';
         return false;
     }
     if (document.login.password.value.length < 6) {
-        document.getElementById("result").innerHTML = "Enter 6 digits minimum";
+        document.getElementById('result').innerHTML = 'Enter 6 digits minimum';
         return false;
     }
-    if (document.login.cpassword.value == "") {
-        document.getElementById("result").innerHTML =
-            "Enter password confirmation";
+    if (document.login.cpassword.value == '') {
+        document.getElementById('result').innerHTML =
+            'Enter password confirmation';
         return false;
     }
     if (document.login.password.value !== document.login.cpassword.value) {
-        document.getElementById("result").innerHTML = "Different password";
+        document.getElementById('result').innerHTML = 'Different password';
         return false;
     }
     if (document.login.password.value == document.login.cpassword.value) {
-        popup.classList.add("open-slide");
+        popup.classList.add('open-slide');
         return false;
     }
 }
 
-const popup = document.getElementById("popup");
+const popup = document.getElementById('popup');
 function closeSlide() {
-    popup.classList.remove("open-slide");
+    popup.classList.remove('open-slide');
 }
